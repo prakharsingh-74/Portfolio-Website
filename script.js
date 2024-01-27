5063158de56530a586a480d288efc71e53834dc7
prakharsingh-74 <prakharsingh7014@gmail.com>
2024-01-27 17:26:39 +0530

    js code updated

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed((".motion"),{
+  strings : ["Frontend Developer", "Machine Learning","UI Designer", "Content Creator", "E-sports Athlete"],
+  typeSpeed : 100,
+  backspeed : 100,
+  backdelay : 1000,
+  loop :  true
+})
