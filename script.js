b4c11c8a2289b15cb14bd2bfa274eb084167959f
prakharsingh-74 <prakharsingh7014@gmail.com>
2024-02-13 17:25:34 +0530

    preloader added

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+{
+  window.addEventListener('load', function () {
+      var preloader = document.getElementById('loading');
+      preloader.style.display = 'none';
+  });
+}
+
 var typed = new Typed((".motion"),{
   strings : ["C++ and DSA","Frontend Developer", "Machine Learning","UI Designer", "Content Creator", "E-sports Athlete"],
   typeSpeed : 100,
