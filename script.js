4696487bb6b30b3597871910c3b513b3e9086ce9
prakharsingh-74 <prakharsingh7014@gmail.com>
2024-02-15 03:25:03 +0530

    preloader working

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-{
-  window.addEventListener('load', function () {
+  document.addEventListener('DOMContentLoaded', function () {
       var preloader = document.getElementById('loading');
+      if (preloader){
       preloader.style.display = 'none';
-  });
-}
+  };
+})
 
 var typed = new Typed((".motion"),{
   strings : ["C++ and DSA","Frontend Developer", "Machine Learning","UI Designer", "Content Creator", "E-sports Athlete"],
