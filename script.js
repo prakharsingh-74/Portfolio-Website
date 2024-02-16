ebe09bfdd1153c21d723d065fda1c71a63f95400
prakharsingh-74 <prakharsingh7014@gmail.com>
2024-02-17 05:29:58 +0530

    preloader removed

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-  document.addEventListener('DOMContentLoaded', function () {
-      var preloader = document.getElementById('loading');
-      if (preloader){
-      preloader.style.display = 'none';
-  };
-})
-
 var typed = new Typed((".motion"),{
   strings : ["C++ and DSA","Frontend Developer", "Machine Learning","UI Designer", "Content Creator", "E-sports Athlete"],
   typeSpeed : 100,
