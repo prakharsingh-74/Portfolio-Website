e8d5e292e2719aed4afd2c56e0cba73ac1e6a133
prakharsingh-74 <prakharsingh7014@gmail.com>
2024-02-17 06:15:21 +0530

    responsiveness added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active'); 
+}
+
 var typed = new Typed((".motion"),{
   strings : ["C++ and DSA","Frontend Developer", "Machine Learning","UI Designer", "Content Creator", "E-sports Athlete"],
   typeSpeed : 100,
@@ -5,3 +14,11 @@ var typed = new Typed((".motion"),{
   backdelay : 1000,
   loop :  true
 })
+
+// sticky header
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
+
+// remove toggle icon and navbar when click navbar link
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
